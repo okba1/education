f5f9f927f63d7e302a60043d7f8d03c4c4479669
Okba <okba.khenissi@insa-lyon.fr>
2015-08-04 00:35:26 +0200

    add principal actions

--- bac/src/FormerStudentsBundle/Controller/BacController.php
+++ bac/src/FormerStudentsBundle/Controller/BacController.php
@@ -8,6 +8,18 @@ class BacController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FormerStudentsBundle:Default:index.html.twig', array('name' => 'okba'));
+        return $this->render('FormerStudentsBundle:Bac:index.html.twig', array('name' => 'okba'));
+    }
+
+    public function inscriptionAction(){
+      return $this->render('FormerStudentsBundle:Bac:inscription.html.twig');
+    }
+
+    public function listAction(){
+      return $this->render('FormerStudentsBundle:Bac:studentList.html.twig');
+    }
+
+    public function documentationAction(){
+      return $this->render('FormerStudentsBundle:Bac:documentation.html.twig');
     }
 }
