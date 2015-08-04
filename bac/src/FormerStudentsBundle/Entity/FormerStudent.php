9f65bb655894da5ab4647d053717eb7e4674e062
Okba <okba.khenissi@insa-lyon.fr>
2015-08-04 22:06:39 +0200

    generate FormerStudent model

---
+++ bac/src/FormerStudentsBundle/Entity/FormerStudent.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace FormerStudentsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * FormerStudent
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class FormerStudent
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastName", type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mail", type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="graduationYear", type="string", length=255)
+     */
+    private $graduationYear;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="university", type="string", length=255)
+     */
+    private $university;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return FormerStudent
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return FormerStudent
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     * @return FormerStudent
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string 
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set graduationYear
+     *
+     * @param string $graduationYear
+     * @return FormerStudent
+     */
+    public function setGraduationYear($graduationYear)
+    {
+        $this->graduationYear = $graduationYear;
+
+        return $this;
+    }
+
+    /**
+     * Get graduationYear
+     *
+     * @return string 
+     */
+    public function getGraduationYear()
+    {
+        return $this->graduationYear;
+    }
+
+    /**
+     * Set university
+     *
+     * @param string $university
+     * @return FormerStudent
+     */
+    public function setUniversity($university)
+    {
+        $this->university = $university;
+
+        return $this;
+    }
+
+    /**
+     * Get university
+     *
+     * @return string 
+     */
+    public function getUniversity()
+    {
+        return $this->university;
+    }
+}
